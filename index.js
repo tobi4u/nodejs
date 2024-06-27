a14c21bdb9ea763f32b7a1e4202c295cb93f7399
balogun michael <balogunmaleek2001@gmail.com>
2024-06-27 12:02:45 +0100

    node

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Hello World!</h1>');
+    res.end();
+}).listen(666);
+
+
+console.log("Server is running on http://localhost:666/");
